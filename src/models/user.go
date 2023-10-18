071e19d61ce35325dbfd98f79c4981baa75f2981
Dmitriy Kozlov <dmitriy.kozlov@neuro-city.ru>
2023-10-18 18:16:51 +0300

    feat: started to implement revenue calculations

--- src/models/user.go
+++ src/models/user.go
@@ -1,16 +1,19 @@
 package models
 
 import (
+	"store/src/database"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	Model
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	Email        string `json:"email" gorm:"unique"`
-	Password     []byte `json:"-"`
-	IsAmbassador bool   `json:"-"`
+	FirstName    string  `json:"first_name"`
+	LastName     string  `json:"last_name"`
+	Email        string  `json:"email" gorm:"unique"`
+	Password     []byte  `json:"-"`
+	IsAmbassador bool    `json:"-"`
+	Revenue      float64 `json:"revenue,omitempty" gorm:"-"`
 }
 
 func (user *User) SetPassword(password string) {
@@ -21,4 +24,31 @@ func (user *User) SetPassword(password string) {
 func (user *User) CheckPassword(password string) error {
 	err := bcrypt.CompareHashAndPassword(user.Password, []byte(password))
 	return err
+}
+
+type Admin User
+
+type Ambassador User
+
+func (ambassador *Ambassador) CalculateRevenue() {
+	var orders []Order
+
+	database.DB.Preload("OrderItems").Find(&orders, &Order{
+		UserId:   ambassador.Id,
+		Complete: true,
+	})
+
+	var revenue float64 = 0
+
+	for _, order := range orders {
+		for _, orderItem := range order.OrderItems {
+			revenue += orderItem.AdminRevenue
+		}
+	}
+
+	ambassador.Revenue = revenue
+}
+
+func (admin *Admin) CalculateRevenue() {
+
 }
