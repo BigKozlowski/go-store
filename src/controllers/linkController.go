df2a7ff6624f1213684dbfa323548c31eb21dc1e
BigKozlowski <dmitriykozlov1994@gmail.com>
2023-10-21 16:01:44 +0300

    feat: ambassador stats endpoint

--- src/controllers/linkController.go
+++ src/controllers/linkController.go
@@ -56,3 +56,38 @@ func CreateLink(c *fiber.Ctx) error {
 
 	return c.JSON(link)
 }
+
+func Stats(c *fiber.Ctx) error {
+	id, _ := middlewares.GetUserId(c)
+
+	var links []models.Link
+
+	database.DB.Find(&links, models.Link{
+		UserId: id,
+	})
+
+	var result []interface{}
+
+	var orders []models.Order
+
+	for _, link := range links {
+		database.DB.Preload("OrderItems").Find(&orders, &models.Order{
+			Code:     link.Code,
+			Complete: true,
+		})
+
+		revenue := 0.0
+
+		for _, order := range orders {
+			revenue += order.GetTotal()
+		}
+
+		result = append(result, fiber.Map{
+			"code":    link.Code,
+			"count":   len(orders),
+			"revenue": revenue,
+		})
+	}
+
+	return c.JSON(result)
+}
--- src/routes/routes.go
+++ src/routes/routes.go
@@ -41,6 +41,6 @@ func Setup(app *fiber.App) {
 	ambassadorAuthenticated.Put("/users/info", controllers.UpdateInfo)
 	ambassadorAuthenticated.Put("/users/password", controllers.UpdatePassword)
 	ambassadorAuthenticated.Post("/links", controllers.CreateLink)
-	// ambassadorAuthenticated.Get("/stats")
+	ambassadorAuthenticated.Get("/stats", controllers.Stats)
 	// ambassadorAuthenticated.Get("/rankings")
 }
